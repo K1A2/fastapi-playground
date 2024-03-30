cf33aab740309dc5b2573a596e4a8ec40df43f07
K1A2 <jckim0307@gmail.com>
2024-03-30 21:16:03 +0900

    feat: mariadb 기본 설정

--- core/config.py
+++ core/config.py
@@ -8,6 +8,14 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
 
     # TODO: SETTING
+    MARIADB_HOST: str = os.getenv("MARIADB_HOST")
+    MARIADB_PORT: str = os.getenv("MARIADB_PORT")
+    MARIADB_USERNAME: str = os.getenv("MARIADB_USERNAME")
+    MARIADB_PASSWORD: str = os.getenv("MARIADB_PASSWORD")
+    MARIADB_DATABASE: str = os.getenv("MARIADB_DATABASE")
+
+    MARIADB_URL: str = (f'mariadb+pymysql://{MARIADB_USERNAME}:{MARIADB_PASSWORD}@{MARIADB_HOST}:{MARIADB_PORT}'
+                        f'/{MARIADB_DATABASE}?charset=utf8mb4')
 
     class Config:
         case_sensitive = True
