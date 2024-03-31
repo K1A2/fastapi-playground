021eb5035307bfe4438c6208b4d94a273cbc9873
K1A2 <jckim0307@gmail.com>
2024-03-31 16:05:23 +0900

    feat: 카카오/구글 엑세스키 검증 추가

---
+++ api/v1/endpoints/oauth.py
@@ -0,0 +1,46 @@
+import hashlib
+
+import httpx
+from fastapi import APIRouter, Depends, Security, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy.orm import Session
+from api.depends import get_mariadb
+from crud.user import *
+
+router = APIRouter()
+
+security = HTTPBearer()
+
+@router.get('/verify/google')
+async def verify_google(
+        credentials: HTTPAuthorizationCredentials = Security(security),
+        db: Session = Depends(get_mariadb)):
+    async with httpx.AsyncClient() as client:
+        result = httpx.get('https://www.googleapis.com/oauth2/v3/userinfo', headers={
+            'Authorization': f'Bearer {credentials.credentials}'
+        })
+
+    if result.status_code != 200:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='Google User Not Found')
+    result = result.json()
+    id = hashlib.sha256(('google' + result['sub']).encode()).hexdigest()
+    print(id)
+
+@router.get('/verify/kakao')
+async def verify_kakao(
+        credentials: HTTPAuthorizationCredentials = Security(security),
+        db: Session = Depends(get_mariadb)):
+    async with httpx.AsyncClient() as client:
+        result = httpx.get('https://kapi.kakao.com/v2/user/me', headers={
+            'Authorization': f'Bearer {credentials.credentials}',
+            'Content-type': f'application/x-www-form-urlencoded;charset=utf-8',
+        })
+
+    if result.status_code != 200:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='Kakao User Not Found')
+
+    result = result.json()
+    id = hashlib.sha256(('kakao' + str(result['id'])).encode()).hexdigest()
+    print(id)
