5f53c0078f650835c138ba679fab3505c99d57e5
K1A2 <jckim0307@gmail.com>
2024-03-31 11:57:14 +0900

    feat: oauth prefix 추가

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import main
+from api.v1.endpoints import main, oauth
 
 api_router = APIRouter()
 api_router.include_router(main.router, prefix="/main", tags=["main"])
+api_router.include_router(oauth.router, prefix="/oauth", tags=["oauth"])
